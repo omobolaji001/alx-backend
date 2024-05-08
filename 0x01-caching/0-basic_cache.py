d6100f4beb3ce1b3c7723cfa587e54d48c739473
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-08 22:43:10 +0100

    Basic Caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Defines BasicCache class that inherits from BaseCaching.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Represents BasicCache"""
+
+    def __init__(self):
+        """Initializes BasicCache"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Adds new item to the Cache"""
+        if key is None or item is None:
+            pass
+        
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the item assigned to key"""
+        if key is None or key not in self.cache_data:
+            return None
+        
+        return self.cache_data.get(key)
